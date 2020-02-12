ed461c51b1a4451aab63d0c33c7fe8f96caa1982
LuiX <luis.a.vivero@gmail.com>
2020-02-11 23:16:32 -0800

    Add Unscented Kalman Filters project

---
+++ sensor-fusion/unscented-kalman-filters/src/tools.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  DONE:
+    * Calculate the RMSE here.
+  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  // check the validity of the following inputs:
+  //  * the estimation vector size should not be zero
+  //  * the estimation vector size should equal ground truth vector size
+  if (estimations.size() == 0 || estimations.size() != ground_truth.size())
+  {
+    std::cout << "Invalid estimation or ground_truth data" << std::endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for (int i = 0; i < estimations.size(); ++i){
+    VectorXd diff = estimations[i] - ground_truth[i];
+    VectorXd mult = diff.array() * diff.array();
+    rmse += mult;
+  }
+
+  //calculate the mean
+  rmse /= estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
+}
